1c35073f88a4e4d4cbdfcec2f4ace758d9b9d1ff
William Kent <wjk011@gmail.com>
2019-07-12 15:55:13 -0400

    Initial commit

---
+++ OokLanguageIntegration/Properties/AddinInfo.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "OokLanguageIntegration",
+    Namespace = "Sunburst",
+    Version = "1.0"
+)]
+
+[assembly: AddinName("Ook Language Binding")]
+[assembly: AddinCategory("Language bindings")]
+[assembly: AddinDescription("Ported to Visual Studio for Mac from the VSSDK sample.")]
+[assembly: AddinAuthor("William Kent")]
